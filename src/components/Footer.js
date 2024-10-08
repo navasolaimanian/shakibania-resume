f6c037b7546148d20a813e15bc38b49ce4e15bf8
navasolaimanian <106958586+navasolaimanian@users.noreply.github.com>
2024-10-08 18:26:32 +0330

    add contact to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { getData } from '../../lib/getData';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer = () => {
-  return (
-    <div className="bg-[#819EB4] h-24">
+  const data = getData('socialMedia.json');
 
+  return (
+    <div className="bg-[#819EB4] h-24 flex items-center justify-center">
+       <ul className="flex items-center [&>*]:mx-1.5 md:[&>*]:mx-3 justify-center">
+                    {data.socialMedias.map(socialMedia =>
+                        <li>
+                            <Link href={socialMedia.link}>
+                                <Image
+                                    alt={socialMedia.alt}
+                                    src={socialMedia.image}
+                                    width="0"
+                                    height="0"
+                                    sizes="100vw"
+                                    className="w-[80%] md:w-full h-auto"
+                                    // style={{ width: '100%', height: 'auto' }}
+                                />
+                            </Link>
+                        </li>
+                    )}
+                </ul>
     </div>
   )
 }
