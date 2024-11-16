c82ff9722c4daf5cb32240ec1ab4ac623e4ff8ca
navasolaimanian <106958586+navasolaimanian@users.noreply.github.com>
2024-11-16 22:16:40 +0330

    increase height of background image of about in lager screen

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -8,7 +8,7 @@ const Introduction = async () => {
     const educationData = getData('educations.json');
 
     return (
-        <div className="relative 3xl:bg-cover bg-no-repeat pt-[4.5rem]" style={{
+        <div className="relative 3xl:bg-cover bg-no-repeat pt-[4.5rem] 3xl:h-[57rem]" style={{
             "backgroundImage": `linear-gradient(rgb(255 255 255 / 92%), rgb(255 255 255 / 92%)), url(${image.src})`,
         }}>
             <div className="grid md:grid-cols-2 container m-auto px-7 lg:pl-10 xl:pl-20 2xl:pl-28 3xl:pl-44 pt-7 3xl:pt-8">
