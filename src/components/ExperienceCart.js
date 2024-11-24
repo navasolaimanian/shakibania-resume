ccb8b30471bcd5b97905fb0a41765eb2074b952c
navasolaimanian <106958586+navasolaimanian@users.noreply.github.com>
2024-11-24 21:19:24 +0330

    Update ExperienceCart.js

--- src/components/ExperienceCart.js
+++ src/components/ExperienceCart.js
@@ -7,7 +7,7 @@ const ExperienceCart = (props) => {
     const [showAll, setShowAll] = useState(false);
     
     return (
-        <div className={`relative bg-[#F3F6F9] hover:shadow-4xl cursor-pointer mb-8 md:mb-11 w-full lg:w-[70%] xl:w-[75%] 2xl:w-[65%] ${props.data.information.length <= 2 ? 'pb-5 md:pb-8 px-3 md:px-5 lg:px-4 pt-3 md:pt-5 lg:pt-4' : 'p-3 md:p-5 lg:p-4'} rounded-lg lg:transition lg:ease-in-out lg:delay-150 lg:hover:-translate-y-1 lg:hover:scale-[1.01]`}>
+        <div className={`relative bg-[#F3F6F9] hover:shadow-4xl cursor-pointer mb-8 md:mb-11 w-full lg:w-[70%] xl:w-[75%] 2xl:w-[65%] ${props.data.information.length <= 2 ? 'pb-4 md:pb-8 px-3 md:px-5 lg:px-4 pt-3 md:pt-5 lg:pt-4' : 'p-3 md:p-5 lg:p-4'} rounded-lg lg:transition lg:ease-in-out lg:delay-150 lg:hover:-translate-y-1 lg:hover:scale-[1.01]`}>
             <div className="flex">
                 <Image alt="" src={robot} className="absolute w-9 md:w-16 h-auto" />
                 <div className="text-[#313C44] ml-12 sm:ml-14 md:ml-20">
@@ -20,7 +20,7 @@ const ExperienceCart = (props) => {
                     <div className="mt-2">
                         {props.data.information.slice(0, 2).map((info, index) =>
                             <div key={index} className="flex mb-1 text-xs sm:text-sm text-[#4D4D4D]">
-                                <span className="w-1 sm:w-1.5 h-1 sm:h-1.5 mt-[7px] sm:mt-2 mr-3 sm:mx-5 bg-[#4D4D4D] rounded-full"></span>
+                                <span className="w-1 sm:w-1.5 h-1 sm:h-1.5 mt-[7px] sm:mt-2 mr-3 sm:mr-5 bg-[#4D4D4D] rounded-full"></span>
                                 <p className="w-[90%] md:w-full">{info}</p>
                             </div>
                         )}
@@ -30,7 +30,7 @@ const ExperienceCart = (props) => {
                             <div>
                                 {props.data.information.slice(2).map((info, index) =>
                                     <div key={index} className="flex mb-1 text-xs sm:text-sm text-[#4D4D4D]">
-                                        <span className="w-1.5 h-1.5 mt-1.5 sm:mt-2 mr-3 sm:mx-5 bg-[#4D4D4D] rounded-full"></span>
+                                        <span className="w-1 sm:w-1.5 h-1 sm:h-1.5 mt-1.5 sm:mt-2 mr-3 sm:mr-5 bg-[#4D4D4D] rounded-full"></span>
                                         <p className="w-[90%] md:w-full">{info}</p>
                                     </div>
                                 )}
@@ -38,7 +38,7 @@ const ExperienceCart = (props) => {
                     </div>
                 </div>
             </div >
-            {props.data.information.length > 2 && <button className="flex text-xs md:text-sm xl:text-base m-auto text-[#959595] my-2 md:my-5" onClick={() => setShowAll(!showAll)}>
+            {props.data.information.length > 2 && <button className="flex text-xs md:text-sm xl:text-base m-auto text-[#959595] my-2 md:mt-3" onClick={() => setShowAll(!showAll)}>
                 {showAll ? 'show less' : 'show more'}
             </button>}
         </div>
